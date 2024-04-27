127c259063104a3707400aefc5250585315ddbc4
rezatahseenfiverr <166533426+rezatahseenfiverr@users.noreply.github.com>
2024-04-27 12:00:10 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-    swcPlugins: [
-      ["next-superjson-plugin", {}]
-    ]
-  },
   images: {
     domains: [
       "res.cloudinary.com",
